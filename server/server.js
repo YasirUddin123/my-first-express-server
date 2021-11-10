5b6ce791cfa62343e0cd8b6488663fbfe344f039
Yasir Uddin <yasir.uddin@icloud.com>
2021-11-10 09:20:14 -0700

    updated server file with kris

--- server/server.js
+++ server/server.js
@@ -11,7 +11,39 @@ const app = express();
     // aka "the client-side" files
     // aka "static assets"
     // aka "all the stuff we've been happily doing already"
+    // GET index.html
 app.use(express.static('./server/public'));
+
+// GET requests with data
+const message = 'Hello from the Server!';
+// Route function, Endpoint
+// req (request), object
+// res (response), object
+app.get('/message', (req, res) => {
+
+    console.log('in the GET route for /message');
+
+    // we need to respond
+    res.send(message);
+});
+
+const dogs = ['Marmaduke', "Snoopy", "Scooby Doo"];
+
+app.get('/dogs', (req, res) => {
+    console.log('in get for /dogs');
+
+
+    //send back the array of data
+    res.send(dogs);
+
+});
+
+app.get('/number', (req, res) => {
+
+    //send a number - numbers are the only data type that will not send
+    res.send(7);
+
+});
 
 // Starts the server, and listens for requests:
 app.listen(5000, function() {
